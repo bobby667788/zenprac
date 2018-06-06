0e83aa27ffed5a5b68454bcd248f1c31f9787550
bobby667788 <gokuverma94@gmail.com>
2018-06-06 10:58:50 +0530

    second

--- june6.js
+++ june6.js
@@ -1,3 +1,28 @@
-hjgjlghlggl
-fhfhgfghfh
-first comit
+var abc='Some skhsu'
+
+
+function car(a,b){
+    if (typeof(a)=='string' && typeof(b)=='string'){
+        return a+b;
+    }
+    else{
+        return 'cant do it';
+    }
+}
+
+console.log(car('bob','ver'));
+console.log(car(1,3));
+
+function split(a){
+    var x;
+    if (typeof(a)=='string'){
+        x=a.split(/\s+/);
+        return x;
+    }
+    else{
+        return 'cant do it';
+    }
+}
+var x=split('bob verma');
+console.log(x[0]);
+console.log(x[1]);
